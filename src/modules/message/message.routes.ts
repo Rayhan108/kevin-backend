39adfa03e69609723d47fbf98a3d98db4c2c3d45
rayhan108 <raihanshorker56@gmail.com>
2025-08-11 16:49:25 +0600

    added message feature

--- src/modules/message/message.routes.ts
+++ src/modules/message/message.routes.ts
@@ -5,10 +5,9 @@ import messageController from './message.controller';
 
 
 const router = express.Router();
-router.use(
-    auth(USER_ROLE.user,USER_ROLE.vipMember,USER_ROLE.contractor,USER_ROLE.vipContractor),);
+
 
 // messageRouter.post('/send', messageControllers.sendMessage);
-router.get('/retrieve/:conversationId', messageController.retrieveAllMessageByConversationId);
+router.get('/retrieve/:conversationId', auth(USER_ROLE.user,USER_ROLE.vipMember,USER_ROLE.contractor,USER_ROLE.vipContractor), messageController.retrieveAllMessageByConversationId);
 
 export const messageRoutes = router;
